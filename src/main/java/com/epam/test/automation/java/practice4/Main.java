ae2ded6e33d40df3b9f4b8b9be09744b86e2049f
Stanislav <Terrozerg@gmail.com>
2021-04-01 13:05:41 +0300

    1

--- src/main/java/com/epam/test/automation/java/practice4/Main.java
+++ src/main/java/com/epam/test/automation/java/practice4/Main.java
@@ -4,9 +4,6 @@ public class Main {
     private Main(){}
 
     public static void main(String[] args) {
-        System.out.println(Task4.sumGeometricElements(5,0.9,0));
-        //System.out.println(Task4.sumGeometricElements(100, 0.5, 0));
-        //System.out.println(Task4.sumGeometricElements(0, 0.5, 20));
-        //throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException();
     }
 }
